bf80ec4f014c62a86fc55540dc1713d1d39eff23
Na3ar-17 <gavruk2017@gmail.com>
2024-05-04 17:21:26 +0200

    added ownerId to project model

---
+++ prisma/migrations/20240504152049_added_owner_id_to_project_model/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "projects" ADD COLUMN     "owner_id" TEXT;
--- src/members/members.service.ts
+++ src/members/members.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMemberDto } from './dto/create-member.dto';
 import { UpdateMemberDto } from './dto/update-member.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class MembersService {}
+export class MembersService {
+  constructor(private prisma: PrismaService) {}
+}
