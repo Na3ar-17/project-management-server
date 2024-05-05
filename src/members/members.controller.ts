ce11b98ba41e76f038e2857f9fbffa3e861b7a8e
Na3ar-17 <gavruk2017@gmail.com>
2024-05-05 13:02:20 +0200

    created endpoint for addMember

--- src/members/members.controller.ts
+++ src/members/members.controller.ts
@@ -11,6 +11,7 @@ import { MembersService } from './members.service';
 import { CreateMemberDto } from './dto/create-member.dto';
 import { UpdateMemberDto } from './dto/update-member.dto';
 import { Auth } from 'src/auth/decorators/auth.decorator';
+import { CurrentUser } from 'src/auth/decorators/user.decorator';
 
 @Controller('members')
 export class MembersController {
@@ -20,5 +21,14 @@ export class MembersController {
   @Get('/all/:projectId')
   async getAll(@Param('projectId') projectId: string) {
     return await this.membersService.getAll(projectId);
+  }
+
+  @Auth()
+  @Post('/add-new/:projectId')
+  async addMember(
+    @CurrentUser('id') userId: string,
+    @Param('projectId') projectId: string,
+  ) {
+    return await this.membersService.addMember(userId, projectId);
   }
 }
