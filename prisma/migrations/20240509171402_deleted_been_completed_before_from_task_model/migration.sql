b6634c517402ec68ae180b3c5d8cf63cd44bd028
Na3ar-17 <gavruk2017@gmail.com>
2024-05-09 19:19:56 +0200

    deleted tasksCreated column from statistics

---
+++ prisma/migrations/20240509171402_deleted_been_completed_before_from_task_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `been_completed_before` on the `tasks` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "tasks" DROP COLUMN "been_completed_before";
